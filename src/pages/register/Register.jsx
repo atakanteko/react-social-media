1c74730803024703014d9a9858bcc6cada2f69bd
atakanteko <atakantekoglu@gmail.com>
2022-11-25 22:01:09 +0300

    register page design

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,43 @@
+const Register = () => {
+    return(
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <h1>Social Life</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab non rerum atque porro maiores dolore enim, voluptatem qui ea dicta nisi facere dolor consequuntur est assumenda maxime nulla voluptatum laudantium.
+                    </p>
+                    <span>Do you have an account?</span>
+                    <button type="button">
+                        Login
+                    </button>
+                </div>
+                <div className="right">
+                    <h1>Register</h1>
+                    <form>
+                        <input 
+                            type="text" 
+                            name="username"
+                            placeholder="Username" 
+                        />
+                        <input 
+                            type="email" 
+                            name="emial"
+                            placeholder="Email" 
+                        />
+                         <input 
+                            type="password" 
+                            name="password"
+                            placeholder="Password" 
+                        />
+                        <button type="submit">
+                            register
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
