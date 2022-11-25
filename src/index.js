92657cc5c0c2ad7980a37e18df4f44b2d5b87892
atakanteko <atakantekoglu@gmail.com>
2022-11-25 20:22:07 +0300

    login page design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./assets/styles/css/social.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,38 @@
+const Login = () => {
+    return(
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab non rerum atque porro maiores dolore enim, voluptatem qui ea dicta nisi facere dolor consequuntur est assumenda maxime nulla voluptatum laudantium.
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <button type="button">
+                        register now
+                    </button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input 
+                            type="text" 
+                            name="username"
+                            placeholder="Username" 
+                        />
+                         <input 
+                            type="password" 
+                            name="password"
+                            placeholder="Password" 
+                        />
+                        <button type="submit">
+                            login
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
