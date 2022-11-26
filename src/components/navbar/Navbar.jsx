27eadecedebbbd76118b1ad57b72e00c61092f0e
atakanteko <atakantekoglu@gmail.com>
2022-11-27 02:16:02 +0300

    navbar design done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,37 @@
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import UserIcon from '../../assets/images/user_icon.png'
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return(
         <div className="navbar">
-            Navbar
+            <div className="left">
+                <Link to="/" style={{textDecoration:'none'}}>
+                    <span>socialapp</span>
+                </Link>
+                <HomeOutlinedIcon />
+                <DarkModeOutlinedIcon />
+                <GridViewOutlinedIcon />
+                <div className="search">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder='Search...' />
+                </div>
+            </div>
+            <div className="right">
+                <PersonOutlineOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsNoneOutlinedIcon />
+                <div className="user">
+                    <img src={UserIcon} alt="user" />
+                    <span>Agah Fikret </span>
+                </div>
+            </div>
         </div>
     )
 }
